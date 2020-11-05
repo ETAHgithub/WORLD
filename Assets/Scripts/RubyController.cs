1858dc523ab0d2b993310daab17a1db0d6a612cf
ETAHgithub <ericgodinez64@gmail.com>
2020-11-05 01:17:32 -0600

    hi

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
@@ -32,7 +33,7 @@ public class RubyController : MonoBehaviour
         currentHealth = maxHealth;
     }
 
-   
+    
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
@@ -60,6 +61,19 @@ public class RubyController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.C))
         {
             Launch();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
         }
     }
 
